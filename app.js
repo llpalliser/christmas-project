d5ba3c05765154960e40d0b1f2c41208d74eb3a2
Llorenç Palliser <palliser@palliser.es>
2020-12-28 08:39:50 +0100

    Structure: done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+// initializations
+
+const app = express();
+
+// settings
+
+// Middlewares
+
+
+// Global variables
+
+
+// Routes
+
+
+// Starting the server
